753cf95bbdcdd1a2be2fa2dadbfa4fb435d7daaf
redisnotbluedev <ededmaude@gmail.com>
2025-11-17 17:45:15 +1030

    more dubeg

--- v0.0.0/files/lib/functools.gd
+++ v0.0.0/files/lib/functools.gd
@@ -1,7 +1,7 @@
 var _methods = {"bind": bind}
 
 func bind(args_array):
-	print(args_array)
+	print("Functools (gd): ", args_array)
 	# args_array = [callback, arg1, arg2, ...]
 	var callback = args_array[0]
 	var bind_args = args_array.slice(1)  # Everything after callback
