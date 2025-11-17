35dd7c69bf9e539fae3b3d9f8e5611af61adbe6e
redisnotbluedev <ededmaude@gmail.com>
2025-11-17 18:00:10 +1030

    BRO IS IT AN ARRAY OR NOT

--- v0.0.0/files/lib/functools.gd
+++ v0.0.0/files/lib/functools.gd
@@ -2,4 +2,4 @@ var _methods = {"bind": bind}
 
 func bind(callback, ...values):
 	print("Functools (gd) values: ", values)
-	return func(args): print("Bound lambda args: ", args, ", bound values: ", values); return callback.call(values + args)
+	return func(...args): print("Bound lambda args: ", args, ", bound values: ", values); return callback.call(values + args)
