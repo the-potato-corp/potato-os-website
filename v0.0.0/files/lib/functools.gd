f110d64893bb3972c97b425c598e915c2ad6ea2d
redisnotbluedev <ededmaude@gmail.com>
2025-11-17 17:43:41 +1030

    WHY THE HELL is this an array hey if it works it works am i right?

--- v0.0.0/files/lib/functools.gd
+++ v0.0.0/files/lib/functools.gd
@@ -1,4 +1,7 @@
 var _methods = {"bind": bind}
 
-func bind(callback, ...values):
-    return func(args): return callback.call(...values + args)
+func bind(args_array):
+	# args_array = [callback, arg1, arg2, ...]
+	var callback = args_array[0]
+	var bind_args = args_array.slice(1)  # Everything after callback
+	return callback.bindv(bind_args)
